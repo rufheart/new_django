2ef4988c6105d8a386d50eaab714c0635d702ae7
github_username <you@domain.com>
2022-04-01 02:08:50 +0400

    django12

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from account.views import *
 from django.conf import settings 
 
 urlpatterns = [
     path('login/', login_user, name='login'),
+    path('logut/', logout_user, name='logout'),
+    path('register/', register_user, name='register',)
 ]
--- account/views.py
+++ account/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('index')
     if request.method == 'POST':
-        name = request.POST.get('name')
+        print('if isledi')
+        uname = request.POST.get('uname')
         password = request.POST.get('password')
-        user = authenticate(request,username=name,password=password)
+        user = authenticate(request,username=uname,password=password)
         if user:
             login(request, user)
             return redirect('index')
@@ -14,3 +17,23 @@ def login_user(request):
             return render(request,'login.html', )  
 
     return render(request,'login.html', {'error':'User or password incorrect'} )
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def register_user(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        lname=request.POST.get('lname')
+        uname=request.POST.get('uname')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+
+        if User.objects.filter(username = uname):
+            return render(request, 'register.html', {"error":"This username using by other user please choose diffrent username"})
+        else:
+            User.objects.create_user(first_name=name, last_name=lname, username=uname, email=email, password=password)
+            return redirect('login')    
+
+    return render(request=request, template_name='register.html')

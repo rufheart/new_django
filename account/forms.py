94142c947353478433ae4d8c14fd6a831ae2ef83
github_username <you@domain.com>
2022-04-06 00:41:31 +0400

    django-14

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,42 @@
 from django import forms
 from django.contrib.auth.models import User
 
+class FormLogin(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = "__all__"
 
+        widgets = {
+            "username":forms.TextInput(attrs={"class":"input-text", "id":"uname", "placeholder":"Username"}),
+            "password":forms.PasswordInput(attrs={"class":"input-text", "id":"pass", "placeholder":"Password"})
+        }
+
+        labels = {
+            "username":"Username",
+            "password":"Password"
+        }
+
+class FormRegister(forms.ModelForm):
+
+    submit = forms.CharField(widget=forms.TextInput(attrs={"type":"submit", "class":"button login","id":"send2", "value":"Submit"}))
+
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+        widgets = {
+            "first_name":forms.TextInput(attrs={"class":"input-text","id":"name", "placeholder":"First Name" }),
+            "last_name":forms.TextInput(attrs={"class":"input-text", "id":"surname", "placeholder":"Last Name"}),
+            "email":forms.TextInput(attrs={"class":"input-text","id":"email", "placeholder":"Email"}),
+            "password":forms.TextInput(attrs={"class":"input-text","id":"pass", "placeholder":"Password"}),
+            "username":forms.TextInput(attrs={"class":"input-text","id":"pass2", "placeholder":"Username"})
+        }
+
+        labels = {
+            "first_name":"First Name",
+            "last_name":"Last Name",
+            "email":"Email",
+            "password":"Password",
+            "username":"Username"
+        }
--- account/views.py
+++ account/views.py
@@ -2,22 +2,43 @@ import email
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from account.forms import FormLogin, FormRegister
 
 def login_user(request):
     if request.user.is_authenticated:
         return redirect('index')
         
     if request.method == 'POST':
-        uname = request.POST.get('uname')
+        username= request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username = uname, password = password)
+        user = authenticate(request, username = username, password = password)
         if user:
             login(request, user)
             return redirect('index')
         else:
             return render(request,'login.html', )  
+    context = {
+        'error':'User or password incorrect',
+        'forms':FormLogin()
+        } 
+    return render(request,'login.html', context)
 
-    return render(request,'login.html', {'error':'User or password incorrect'} )
+
+# def login_user(request):
+#     if request.user.is_authenticated:
+#         return redirect('index')
+        
+#     if request.method == 'POST':
+#         uname = request.POST.get('uname')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username = uname, password = password)
+#         if user:
+#             login(request, user)
+#             return redirect('index')
+#         else:
+#             return render(request,'login.html', )  
+
+#     return render(request,'login.html', {'error':'User or password incorrect'} )    
 
 def logout_user(request):
     logout(request)
@@ -25,9 +46,9 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        name=request.POST.get('name')
-        lname=request.POST.get('lname')
-        uname=request.POST.get('uname')
+        name=request.POST.get('first_name')
+        lname=request.POST.get('last_name')
+        uname=request.POST.get('username')
         email=request.POST.get('email')
         password=request.POST.get('password')
 
@@ -37,4 +58,8 @@ def register_user(request):
             User.objects.create_user(first_name=name, last_name=lname, username=uname, email=email, password=password)
             return redirect('login')    
 
-    return render(request=request, template_name='register.html')
+    context ={
+        "forms":FormRegister()
+    }
+
+    return render(request=request, template_name='register.html',context=context)
